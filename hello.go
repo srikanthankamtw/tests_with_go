3fb5f79273d046dc15fb7da5f90d33097a3b44dc
Ankam Satya Sri Kanth <satyasrikanth.ankam@thoughtworks.com>
2022-10-08 19:39:13 +0530

    refactor. hello and hello_test file

--- hello.go
+++ hello.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -5,8 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	actual := Hello("Satya")
-	expected := "Hello, Satya"
+	t.Run("say Hello with name", func(t *testing.T) {
+		actual := Hello("Satya")
+		expected := "Hello, Satya"
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("say Hello, world when empty string supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, world"
+		assertCorrectMessage(t, actual, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, actual string, expected string) {
+	t.Helper()
 	if expected != actual {
 		t.Errorf("Expceted %q, Actual %q", expected, actual)
 	}
